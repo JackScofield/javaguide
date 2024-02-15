d95fdc3b23221218671e2aedb9323fb7508b0af0
JackScofield <apuaimanlian@gmail.com>
2024-02-15 14:27:00 -0500

    fix some bugs

--- code/src/main/java/org/example/Main.java
+++ code/src/main/java/org/example/Main.java
@@ -3,9 +3,6 @@ package org.example;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-
-import static org.example.Main.minJump;
 
 public class Main {
 
